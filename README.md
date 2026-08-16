4aa356340da8b9fc8fd01bedf9bcb89749cf3b0e
Ahmad Roisul <ahmadroisul412@gmail.com>
2026-08-16 15:51:14 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Seed of Destiny
+
+Biji mentimun, jarum, garam, dan terasi bukan sekadar "sihir", tetapi dipelajari mekanismenya secara ilmiah (misal: reaksi osmosis garam, sifat fisik tanaman hias/hutan Indonesia). Pemain harus mengelola sumber daya alam Nusantara untuk memecahkan puzzle rintangan.
---
+++ characters/mbok_rondo.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+
+const SPEED = 10
+const JUMP_VELOCITY = 4.5
+
+@onready var animstate: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
+
+	
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	$Sprite3D.rotation.z = lerpf($Sprite3D.rotation.z, -velocity.x / 50, 0.2)
+	
+	$AnimationTree.set("parameters/Walk/blend_position", Vector2(velocity.x, velocity.z))
+	
+	move_and_slide()
